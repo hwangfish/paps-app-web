c111738f643be7d0e0762b35d116dfa5460524f6
hwangfish <jennifern13579@gmail.com>
2026-06-09 10:39:51 +0900

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { BrowserRouter } from 'react-dom'
+import { BrowserRouter } from 'react-router-dom' // 👈 'react-router-dom'으로 정확히 수정했습니다!
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
